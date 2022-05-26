27831b7b42bfbc5a1d981f85db4b57e6ef4554ed
YADIN PAULO MANRIQUE MARQUEZ <ypmanrique2@misena.edu.co>
2022-05-26 18:58:54 -0500

    codificar función de transición

--- app.js
+++ app.js
@@ -13,16 +13,24 @@ var $html = {
     adivinado: document.querySelector('.adivinado'),
     errado: document.querySelector('.errado',)
 }
+
 function dibujar(juego) {
     // Actualizar la imagen del hombre
     var $elem
     $elem = $html.hombre
-    $elem.src = './img/status/0' + juego.status + '.svg'
+    
+    var status  = juego.status
+    if (status === 8) {
+        status = juego.previo
+    }
+    $elem.src = './img/status/0' + status + '.svg'
 
     // Creamos las letras adivinadas
     var palabra = juego.palabra
     var adivinado = juego.adivinado
     $elem = $html.adivinado
+    // Borramos los elementos anteriores
+    $elem.innerHTML = ''
     for (let letra of palabra) {
         let $span = document.createElement('span')
         let $txt = document.createTextNode('')
@@ -37,6 +45,8 @@ function dibujar(juego) {
     // Creamos las letras erradas
     var errado = juego.errado
     $elem = $html.errado
+    // Borramos los elementos anteriores
+    $elem.innerHTML = ''
     for (let letra of errado) {
         let $span = document.createElement('span')
         let $txt = document.createTextNode(letra)
@@ -47,7 +57,57 @@ function dibujar(juego) {
     }
 }
 
-console.log(juego);
+function adivinar(juego, letra) {
+    var status = juego.status
+    // Si ya se ha perdido, o ganado, no hay que hacer nada 
+    if (status === 1 || status === 8 ) {
+        return
+    }
+
+    var adivinado = juego.adivinado
+    var errado = juego.errado
+    // Si ya hemos adivinado o errado la letra, no hay que hacer nada 
+    if (adivinado.indexOf(letra) >= 0 || 
+        errado.indexOf(letra) >= 0) {
+        return
+    }
+
+    var palabra = juego.palabra
+    // Si es letra de la palabra, 
+    if (palabra.indexOf(letra) >= 0) {
+        let ganado = true
+        // Debemos ver si llegamos al estado ganador 
+        for (let l of palabra) {
+            if (adivinado.indexOf(l) < 0 && l != letra) {
+                ganado = false
+                juego.previo = juego.status
+                break
+            }
+        }
+        // Si ya se ha ganado, debemos indicarlo 
+        if (ganado) {
+            juego.status = 8
+        }
+        // Agregamos la letra, a la lista de letras adivinadas
+        adivinado.push(letra)
+    } else {
+        // Si no es letra de la palabra, acercamos al hombre un paso más de su horca
+        juego.status--
+        // Agregamos la letra, a la lista de letras erradas
+        errado.push(letra)
+    }
+}
+
+window.onkeypress = function adivinarLetra(e) {
+    var letra = e.key
+    letra = letra.toUpperCase()
+    if (/[^A-ZÑ]/.test(letra)) {
+        return
+    }
+    adivinar(juego, letra)
+    dibujar(juego)
+}
+
 dibujar(juego)
 
 }())
